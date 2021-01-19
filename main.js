85d19d898ee97155a9218a913ea6b9758fc83b12
Jiseon Kim <jsid1561@gmail.com>
2021-01-19 15:25:34 +0900

    Make clothes filter by type

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
-// Buttons
+// Navbar buttons
 let blue = document.getElementById('blue');
 let yellow = document.getElementById('yellow');
 let pink = document.getElementById('pink');
+let tshirt = document.getElementById('tshirt');
+let skirt = document.getElementById('skirt');
+let pants = document.getElementById('pants');
 
-// Clothes
+// Clothes by color
 let blueClothes = document.getElementsByClassName('blue');
 let pinkClothes = document.getElementsByClassName('pink');
 let yellowClothes = document.getElementsByClassName('yellow');
 
+// Clothes by type
+let tshirtClothes = document.getElementsByClassName('tshirt');
+let pantsClothes = document.getElementsByClassName('pants');
+let skirtClothes = document.getElementsByClassName('skirt');
 
+
+// logo
+let logo = document.getElementById('logo');
+
+
+// Click logo
+logo.onclick = function(){
+
+    for(let i=0; i<yellowClothes.length; i++){
+        blueClothes[i].style.display = 'flex';
+    }
+    for(let i=0; i<yellowClothes.length; i++){
+        pinkClothes[i].style.display = 'flex';
+    }
+    for(let i=0; i<yellowClothes.length; i++){
+        yellowClothes[i].style.display = 'flex';
+    }
+}
+
+
+// Filtering by color
 blue.onclick = function(){
-    
+
     for(let i=0; i<yellowClothes.length; i++){
         blueClothes[i].style.display = 'flex';
     }
@@ -45,5 +73,47 @@ yellow.onclick = function(){
     }
     for(let i=0; i<yellowClothes.length; i++){
         yellowClothes[i].style.display = 'flex';
+    }
+}
+
+
+
+// Filtering by type
+tshirt.onclick = function(){
+
+    for(let i=0; i<yellowClothes.length; i++){
+        tshirtClothes[i].style.display = 'flex';
+    }
+    for(let i=0; i<pinkClothes.length; i++){
+        skirtClothes[i].style.display = 'none';
+    }
+    for(let i=0; i<yellowClothes.length; i++){
+        pantsClothes[i].style.display = 'none';
+    }
+}
+
+skirt.onclick = function(){
+
+    for(let i=0; i<yellowClothes.length; i++){
+        tshirtClothes[i].style.display = 'none';
+    }
+    for(let i=0; i<pinkClothes.length; i++){
+        skirtClothes[i].style.display = 'flex';
+    }
+    for(let i=0; i<yellowClothes.length; i++){
+        pantsClothes[i].style.display = 'none';
+    }
+}
+
+pants.onclick = function(){
+
+    for(let i=0; i<yellowClothes.length; i++){
+        tshirtClothes[i].style.display = 'none';
+    }
+    for(let i=0; i<pinkClothes.length; i++){
+        skirtClothes[i].style.display = 'none';
+    }
+    for(let i=0; i<yellowClothes.length; i++){
+        pantsClothes[i].style.display = 'flex';
     }
 }

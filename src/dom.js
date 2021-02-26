f12348b46b3fa31ef870a81d1e1ce46a14b5a0ca
Jiseon Kim <jsid1561@gmail.com>
2021-02-26 14:24:39 +0900

    refactor: 불필요한 코드 삭제

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,3 @@
-export const $blueBtn = document.getElementById("blue");
-export const $yellowBtn = document.getElementById("yellow");
-export const $pinkBtn = document.getElementById("pink");
-export const $tshirtBtn = document.getElementById("t-shirt");
-export const $skirtBtn = document.getElementById("skirt");
-export const $pantsBtn = document.getElementById("pants");
-
 export const $main = document.querySelector(".main");
 export const $navbar = document.querySelector(".navbar");
 export const $logoContainer = document.querySelector(".logo-container");

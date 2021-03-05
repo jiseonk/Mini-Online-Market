51553a58ae911bb1344bd33e996c3549fd71d7d5
Jiseon Kim <jsid1561@gmail.com>
2021-03-05 14:49:10 +0900

    feature: main.js에 logo 이벤트 등록

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { loadItems } from "./loadItem.js";
-import { $navbar } from "./dom.js";
+import { $logoContainer, $navbar } from "./dom.js";
 import { filter } from "./filter.js";
+import { showAllList } from "./logo.js";
 
 const app = async () => {
   await loadItems();
 
   $navbar.addEventListener("click", filter);
+  $logoContainer.addEventListener("click", showAllList);
 };
 
 app();
